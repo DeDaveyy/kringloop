cc15d95670b06bd686dcf07497c2f459a82bf840
TeunHSytrius <TeunHSytrius@users.noreply.github.com>
2024-02-01 14:30:55 +0100

    Login functie

---
+++ functions/login.function.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Login functie 
+ * Deze functie is voor het inloggen die in de [pages/login.php] staat.
+ */
+
+ require "./classes/dbh.class.php";
+
+$dbconn = new Dbh;
+$dbconn->getConnection(); //Pakt de connectie van de database
+
+//Begin login functie
+
+function login() {
+   
+}
