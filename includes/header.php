87e8de902188685a30bed16afa18f0172a6ff8e2
DeDaveyy <qivvyt@gmail.com>
2024-02-01 12:52:45 +0100

    updated

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Your Page Title</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
@@ -12,6 +13,9 @@
         }
         .navbar-custom a {
             color: #fff !important; /* White text color */
+        }
+        .navbar-custom .dropdown-item {
+            color: #000 !important; /* Black text color for dropdown item */
         }
         .custom-button {
             background-color: #007bff; /* Blue button color */
@@ -25,13 +29,23 @@
 
 <nav class="navbar navbar-expand-lg navbar-custom">
     <a class="navbar-brand" href="#">Kringloop centrum</a>
+    <!-- Aanmelden button -->
+    <a class="btn custom-button" href="aanmelden.php">Aanmelden</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link" href="home.php">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="ritten.php">Ritten</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Ritten
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="ritten_planning.php">Ritten Planning</a>
+                </div>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="voorraad.php">Voorraad</a>
@@ -42,10 +56,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="admin.php">Admin</a>
             </li>
-            <a class="btn custom-button" href="aanmelden.php">Aanmelden</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </ul>
     </div>
 </nav>
