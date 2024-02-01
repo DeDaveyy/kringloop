d4227c497087063f5ae00306bead4dcc739de908
TeunHSytrius <teunhak2003@gmail.com>
2024-02-01 12:51:49 +0100

    Comments toegevoegd

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,21 +1,26 @@
 <?php
 
 class Dbh {
+
+    //Properties van de database.
     private $host = "localhost";
     private $user = "root";
     private $pwd = "";
     private $dbName = "duurzaam";
 
-    // Get the connection
+    // Pakt de connectie
     public function getConnection() {
         return $this->connect();
     }
 
-    // Create the connection
+    // Maakt een connectie
     protected function connect() {
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
         $pdo = new PDO($dsn, $this->user, $this->pwd);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+       // echo "Connectie gelukt";
+
         return $pdo;
     }
 }
