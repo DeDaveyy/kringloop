cc2a776f96d676bf7e9a8fb5c0c2fb502fb981c6
DeDaveyy <qivvyt@gmail.com>
2024-02-02 08:48:42 +0100

    toch wel

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
-include ("./includes/header.includes.php");
+include "./includes/header.includes.php";
 ?>
 
 <head>
+<style>
+  /* Style voor achtergrond- en tekstkleur */
+    .card {
+      background-color: #007bff;
+      color: #fff; 
+    }
+</style>
 </head>
 <main class="mt-3">
 <div class="container mt-5">
   <div class="row justify-content-center">
-    <!-- First Column -->
+    <!-- Eerste Kolom -->
     <div class="col-md-6">
       <div class="card mb-3">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Ritten</h5>
-          <button class="btn btn-primary"><a href="#destination1" class="text-white">Ga naar ritten</a></button>
+          <button class="btn btn-primary"><a href="#destination1" class="text-white">Ga naar het rit overzicht</a></button>
         </div>
       </div>
       
       <div class="card">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Voorraad beheer</h5>
-          <button class="btn btn-primary"><a href="#destination2" class="text-white">Ga naar voorraad beheer</a></button>
+          <button class="btn btn-primary"><a href="#destination2" class="text-white">Go to Destination</a></button>
         </div>
       </div>
     </div>
 
-    <!-- Second Column -->
+    <!-- Tweede Kolom -->
     <div class="col-md-6">
       <div class="card mb-3">
         <div class="card-body d-flex flex-column align-items-center">
@@ -36,7 +43,7 @@ include ("./includes/header.includes.php");
       <div class="card">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Klanten</h5>
-          <button class="btn btn-primary"><a href="#destination4" class="text-white">Ga naar klanten</a></button>
+          <button class="btn btn-primary"><a href="#destination4" class="text-white">Ga naar de klant pagina</a></button>
         </div>
       </div>
     </div>
