61f93fed7777299bd675834f655cb7017e13839e
DeDaveyy <qivvyt@gmail.com>
2024-02-02 09:02:35 +0100

    das

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-include "./includes/header.includes.php";
+include ("./includes/header.includes.php");
 ?>
 
 <head>
 <style>
   /* Style voor achtergrond- en tekstkleur */
     .card {
-      background-color: #007bff;
-      color: #fff; 
+      background-color: #fff;
     }
 </style>
 </head>
@@ -19,6 +18,7 @@ include "./includes/header.includes.php";
       <div class="card mb-3">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Ritten</h5>
+          <h6>asddas<br/></h6>
           <button class="btn btn-primary"><a href="#destination1" class="text-white">Ga naar het rit overzicht</a></button>
         </div>
       </div>
@@ -26,6 +26,7 @@ include "./includes/header.includes.php";
       <div class="card">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Voorraad beheer</h5>
+          <h6>asddas<br/></h6>
           <button class="btn btn-primary"><a href="#destination2" class="text-white">Go to Destination</a></button>
         </div>
       </div>
@@ -36,6 +37,7 @@ include "./includes/header.includes.php";
       <div class="card mb-3">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Kledingstukken</h5>
+          <h6>asddas<br/></h6>
           <button class="btn btn-primary"><a href="#destination3" class="text-white">Ga naar kledingstukken</a></button>
         </div>
       </div>
@@ -43,6 +45,7 @@ include "./includes/header.includes.php";
       <div class="card">
         <div class="card-body d-flex flex-column align-items-center">
           <h5 class="card-title">Klanten</h5>
+          <h6>asddas<br/></h6>
           <button class="btn btn-primary"><a href="#destination4" class="text-white">Ga naar de klant pagina</a></button>
         </div>
       </div>
